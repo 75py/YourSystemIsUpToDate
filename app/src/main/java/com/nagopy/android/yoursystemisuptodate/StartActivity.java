38e6c1ac41e9a2487025eac78beecf10473ff96a
75py <dev.75py@gmail.com>
2018-01-04 13:09:08 +0900

    Open the system update screen directly

--- app/src/main/java/com/nagopy/android/yoursystemisuptodate/StartActivity.java
+++ app/src/main/java/com/nagopy/android/yoursystemisuptodate/StartActivity.java
@@ -2,18 +2,35 @@ package com.nagopy.android.yoursystemisuptodate;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class StartActivity extends Activity {
+
+    private static final String ACTION_SYSTEM_UPDATE_SETTINGS = "android.settings.SYSTEM_UPDATE_SETTINGS";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent systemUpdateActivity = new Intent("android.settings.SYSTEM_UPDATE_SETTINGS");
-        startActivity(systemUpdateActivity);
+        Intent systemUpdateActivity = new Intent(ACTION_SYSTEM_UPDATE_SETTINGS);
+        startActivityIfLaunchable(systemUpdateActivity);
 
         finish();
+    }
+
+    void startActivityIfLaunchable(Intent intent) {
+        PackageManager packageManager = getPackageManager();
+        List<ResolveInfo> result = packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+        if (result.isEmpty()) {
+            Toast.makeText(this, "Failed to open the system update screen.", Toast.LENGTH_SHORT).show();
+        } else {
+            startActivity(intent);
+        }
     }
 }
 
